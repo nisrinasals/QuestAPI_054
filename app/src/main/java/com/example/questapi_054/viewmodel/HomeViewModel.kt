7086d54256e1db36a8db0fb3d42fabf3353685b5
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-18 19:40:28 +0700

    buat HomeViewModel.kt

---
+++ app/src/main/java/com/example/questapi_054/viewmodel/HomeViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.questapi_054.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_054.modeldata.DataSiswa
+import com.example.questapi_054.repositori.RepositoriDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+sealed interface StatusUiSiswa{
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoriDataSiswa: RepositoriDataSiswa):
+    ViewModel(){
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+
+}
