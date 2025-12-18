2a4ee4d8e77f024607067d844e90c7ad3cc8df4a
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-18 20:06:25 +0700

    tambah icon untuk gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
+    implementation(libs.compose.icons)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
