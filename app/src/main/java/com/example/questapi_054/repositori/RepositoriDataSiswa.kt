549d618f15b5b55230f7fef8799108a98cc732cb
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 11:38:53 +0700

    menambah fun getSatuSiswa pada class JaringanRepositiri

--- app/src/main/java/com/example/questapi_054/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_054/repositori/RepositoriDataSiswa.kt
@@ -18,4 +18,5 @@ class JaringanRepositoriDataSiswa(
 ): RepositoriDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
