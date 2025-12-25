c14209efb89c4aa8c64988f190db91fc959635e2
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 11:37:34 +0700

    menambah suspend fun hapusSatuSiswa pada interface RepositoriDataSiswa

--- app/src/main/java/com/example/questapi_054/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_054/repositori/RepositoriDataSiswa.kt
@@ -9,6 +9,7 @@ interface RepositoriDataSiswa{
     suspend fun  postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
     suspend fun getSatuSiswa(id: Int) : DataSiswa
     suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int) : retrofit2.Response<Void>
 
 }
 
