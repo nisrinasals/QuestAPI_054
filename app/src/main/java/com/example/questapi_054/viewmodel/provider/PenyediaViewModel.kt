1f8fb69699dd8823a1f778a10002a7788a50fcda
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 12:39:04 +0700

    add initializer buat DetailVM dan EditVM

--- app/src/main/java/com/example/questapi_054/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_054/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.questapi_054.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_054.repositori.AplikasiDataSiswa
+import com.example.questapi_054.viewmodel.DetailViewModel
+import com.example.questapi_054.viewmodel.EditViewModel
 import com.example.questapi_054.viewmodel.EntryViewModel
 import com.example.questapi_054.viewmodel.HomeViewModel
 
@@ -16,5 +19,17 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoriDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoriDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoriDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoriDataSiswa
+            )
+        }
     }
 }
