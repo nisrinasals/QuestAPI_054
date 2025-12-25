5f44eae7f9b5b5d3e008b36511221239a735f585
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 12:35:12 +0700

    suspend fun hapusSatuSiswa pada DetailviewModel.kt

--- app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
+++ app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
@@ -1,5 +1,6 @@
 package com.example.questapi_054.viewmodel
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,6 +12,7 @@ import com.example.questapi_054.repositori.RepositoriDataSiswa
 import com.example.questapi_054.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
+import retrofit2.Response
 import java.io.IOException
 
 sealed interface StatusUIDetail {
@@ -42,6 +44,17 @@ class DetailViewModel(
             } catch (e: HttpException) {
                 StatusUIDetail.Error
             }
+        }
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    suspend fun hapusSatuSiswa() {
+        val resp: Response<Void> = repositoriDataSiswa.hapusSatuSiswa(idSiswa)
+
+        if (resp.isSuccessful) {
+            println("Sukses Hapus Data : ${resp.message()}")
+        } else {
+            println("Gagal Hapus Data : ${resp.errorBody()}")
         }
     }
 }
