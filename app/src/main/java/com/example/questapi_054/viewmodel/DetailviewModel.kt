fa852812f25516e13dc74c631d0b62190ffea322
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 12:33:10 +0700

    variabel pada DetailviewModel.kt

--- app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
+++ app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
@@ -1,9 +1,13 @@
 package com.example.questapi_054.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.questapi_054.modeldata.DataSiswa
 import com.example.questapi_054.repositori.RepositoriDataSiswa
+import com.example.questapi_054.uicontroller.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
@@ -14,4 +18,9 @@ sealed interface StatusUIDetail {
 class DetailViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoriDataSiswa: RepositoriDataSiswa
-) : ViewModel()
+) : ViewModel(){
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+}
