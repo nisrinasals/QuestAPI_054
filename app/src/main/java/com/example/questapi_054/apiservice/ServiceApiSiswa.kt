a36c95ab0d6d8698533e5ea1eb6428c09d167220
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 11:30:59 +0700

    menambah delete  satu siswa pada service api

--- app/src/main/java/com/example/questapi_054/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_054/apiservice/ServiceApiSiswa.kt
@@ -19,4 +19,7 @@ interface ServiceApiSiswa{
 
     @PUT("editTM.php/{id}")
     suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa) :retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrifit2.Response<Void>
 }
