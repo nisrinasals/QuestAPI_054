8f992693fd18cf5b1a509edd0b0f824f5f90e76b
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 12:29:46 +0700

    tambah suspend fun editSatuSiswa pada EditViewModel.kt

--- app/src/main/java/com/example/questapi_054/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_054/viewmodel/EditViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questapi_054.modeldata.DetailSiswa
 import com.example.questapi_054.modeldata.UIStateSiswa
+import com.example.questapi_054.modeldata.toDataSiswa
 import com.example.questapi_054.modeldata.toUiStateSiswa
 import com.example.questapi_054.repositori.RepositoriDataSiswa
 import com.example.questapi_054.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class EditViewModel(
     savedStateHandle: SavedStateHandle,
@@ -39,6 +41,21 @@ class EditViewModel(
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            val call: Response<Void> = repositoriDataSiswa.editSatuSiswa(
+                idSiswa,
+                uiStateSiswa.detailSiswa.toDataSiswa()
+            )
+
+            if (call.isSuccessful) {
+                println("Update Sukses : ${call.message()}")
+            } else {
+                println("Update Error : ${call.errorBody()}")
+            }
         }
     }
 }
