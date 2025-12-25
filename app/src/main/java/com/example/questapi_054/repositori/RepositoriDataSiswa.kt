8db983686a0e0f13e68ee7a9707bba733c9ec0ed
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 11:40:12 +0700

    menambah fun hapusSatuSiswa pada class JaringanRepositiri

--- app/src/main/java/com/example/questapi_054/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_054/repositori/RepositoriDataSiswa.kt
@@ -20,4 +20,5 @@ class JaringanRepositoriDataSiswa(
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
     override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
