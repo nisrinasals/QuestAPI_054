62dde4b101340959e4fa7489de666dd114ea1664
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 13:21:28 +0700

    menambah argumen pada DestinasiEdit.kt

--- app/src/main/java/com/example/questapi_054/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_054/uicontroller/route/DestinasiEdit.kt
@@ -3,6 +3,8 @@ package com.example.questapi_054.uicontroller.route
 import com.example.questapi_054.R
 
 object DestinasiEdit : DestinasiNavigasi {
-    override val route = "item_edit"
+    override val route = "edit_siswa"
     override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
