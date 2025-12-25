d64a90eb9d6612e9f2ba67379540b3b5cdf6bc65
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 12:34:30 +0700

    fun getSatuSiswa pada DetailviewModel.kt

--- app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
+++ app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
@@ -5,9 +5,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_054.modeldata.DataSiswa
 import com.example.questapi_054.repositori.RepositoriDataSiswa
 import com.example.questapi_054.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
@@ -23,4 +27,21 @@ class DetailViewModel(
     private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
     var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoriDataSiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: HttpException) {
+                StatusUIDetail.Error
+            }
+        }
+    }
 }
