8a6e5083d0ff204fd1cca0cbea2125b2a3db19c3
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 12:27:15 +0700

    tambah variabel yg digunakan pada EditViewModel.kt

--- app/src/main/java/com/example/questapi_054/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_054/viewmodel/EditViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.questapi_054.viewmodel
 
-class EditViewModel() {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.questapi_054.modeldata.UIStateSiswa
+import com.example.questapi_054.repositori.RepositoriDataSiswa
+import com.example.questapi_054.uicontroller.route.DestinasiDetail
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriDataSiswa: RepositoriDataSiswa
+) : ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
 }
