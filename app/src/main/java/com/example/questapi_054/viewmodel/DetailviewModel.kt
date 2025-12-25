10d7051c8eb2c7af5bd8c539d1cb29f0edffb296
nisrina salsa <nisrinahikmawan@gmail.com>
2025-12-25 12:31:23 +0700

    tambah sealed interface StatusUIDetail DetailviewModel.kt

--- app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
+++ app/src/main/java/com/example/questapi_054/viewmodel/DetailviewModel.kt
@@ -1,4 +1,12 @@
 package com.example.questapi_054.viewmodel
 
+import com.example.questapi_054.modeldata.DataSiswa
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
 class DetailviewModel {
 }
